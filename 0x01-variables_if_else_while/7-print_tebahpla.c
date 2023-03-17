ef6a329bcd94d6a698fad0d384f3235f88b541ea
Shady Ahmed <shadyahmed9719@gmail.com>
2023-03-17 10:36:33 -0700

    update c file

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	char low;
+        char low;
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);        
-        printf("\n");	
+        for (low = 'z'; low <= 'a'; low--)
+                putchar(low);
+        printf("\n");
 
-	return (0);
+        return (0);
 }
