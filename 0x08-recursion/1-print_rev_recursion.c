6619a35def4bc1183dde747d21c435851a3da68f
Shady Ahmed <shadyahmed9719@gmail.com>
2023-04-05 11:39:42 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _print_rev_recursion - a function that prints a string in reverse,
+ * followed by a new line.
+ * @s: An input string to printing
+ * Return: Nothing
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _strlen_recursion - a function that returns the length of a string.
+ * @s: An input string to printing
+ * Return: The length of the string
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s)
